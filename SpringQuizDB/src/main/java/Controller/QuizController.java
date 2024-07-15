20e56bb73331a43f6c3e46324bb42dd803fd1121
monikagunas <monikaguna2000@gmail.com>
2024-07-15 19:27:27 +0530

    Comment Added

--- SpringQuizDB/src/main/java/Controller/QuizController.java
+++ SpringQuizDB/src/main/java/Controller/QuizController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+//create Quiz controller
 import Entity.QuizDetails;
 import ServiceImpl.QuizServiceImpl;
 
